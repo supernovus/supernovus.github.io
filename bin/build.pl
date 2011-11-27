9fad29cb7887b085b2f858974325d4bbb3ae9ad9
Timothy Totten <2009@huri.net>
2011-11-27 11:33:48 -0800

    Massive update. Page looks way better, now generated with Garden.

---
+++ bin/build.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+
+use Modern::Perl;
+use Garden;
+use JSON;
+
+sub slurp {
+  my $filename = shift;
+  my $text = do { local( @ARGV, $/ ) = $filename ; <> } ;
+  return $text;
+}
+
+sub output_html {
+  my ($page, $html) = @_;
+  my $outfile = "$page.html";
+  open(my $file, '>', $outfile);
+  print $file $html;
+  close $file;
+}
+
+my $config = decode_json(slurp('./src/config.json'));
+my $garden = Garden->new(paths=>['./src']);
+
+## Okay, now let's build the statuses data.
+my $statuses = $config->{statuses};
+my $projects = $config->{projects};
+## Sort projects into appropriate categories.
+my $stat = {}; ## A cache of statuses, used to populate projects.
+for my $status (@{$statuses}) {
+  my $sid = $status->{id};
+  $status->{items} = []; ## Here we shall store our items.
+  $stat->{$sid} = $status;
+}
+for my $pid (sort keys %{$projects}) {
+  my $project = $projects->{$pid};
+  if (!exists $project->{links}) {
+    $project->{src} = $pid;
+  }
+  my $is = $project->{is};
+  push(@{$stat->{$is}->{items}}, $project);
+}
+
+say "Building page.";
+
+my $updated = $config->{updated};
+my $tocpage = $garden->get('index');
+my $tochtml = $tocpage->render(updated=>$updated, statuses=>$statuses);
+output_html('index', $tochtml);
+
+say "Done building page.";
+
