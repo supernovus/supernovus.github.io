dd7e5021cf1a93ae213657a3ca7496c052126a75
Timothy Totten <2010@huri.net>
2022-07-18 20:10:39 -0700

    WIP on new page

---
+++ README.md
@@ -0,0 +1,12 @@
+# supernovus.github.io
+
+This repository holds the files for http://supernovus.github.io/
+
+It used to be built with some of my ancient Perl 5 libraries.
+I will likely write something new to replace that at some point,
+for now this will simply host a simple HTML page and some compiled
+API documentation from my various Javascript and PHP libraries.
+
+My Perl 5 and Raku (formerly Perl 6) stuff is all so old I'm not going
+to bother even listing it in here.
+
---
+++ scripts/projects.js
@@ -0,0 +1,47 @@
+(function()
+{
+  "use strict";
+
+  const list = document.querySelector("#projects > tbody");
+  const tmpl = document.querySelector("#project");
+
+  function renderProject(proj, data)
+  {
+    const item = tmpl.content.cloneNode(true);
+    const lang = item.querySelector('.lang > code');
+    lang.textContent = proj.lang;
+    lang.classList.add(proj.lang);
+    item.querySelector('.name').textContent = proj.name;
+    item.querySelector('.desc').textContent = proj.desc;
+    const srcLink = item.querySelector('.src a');
+    const docsLink = item.querySelector('.docs a');
+    srcLink.setAttribute('href', data.srcurl+proj.src);
+    docsLink.setAttribute('href', data.docurl+proj.docs);
+    list.appendChild(item);
+  }
+
+  function renderProjects(data)
+  {
+    if (data.title)
+    {
+      document.querySelector('head > title').textContent = data.title;
+      document.querySelector('body > h1').textContent = data.title;
+    }
+    if (data.updated)
+    {
+      document.querySelector('#updated').textContent = data.updated;
+    }
+    if (Array.isArray(data.projects))
+    {
+      for (const proj of data.projects)
+      {
+        renderProject(proj, data);
+      }
+    }
+  }
+
+  fetch('/src/projects.json')
+    .then(resp => resp.json())
+    .then(renderProjects);
+
+})();
